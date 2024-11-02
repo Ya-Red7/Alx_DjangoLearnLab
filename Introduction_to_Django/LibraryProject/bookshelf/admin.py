fe96fca4db13e1f534b3de37d0cca1119b7c0f0a
Yared W <yaredwondatir77@gmail.com>
2024-11-02 22:42:04 +0300

    push task 3

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,5 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
+    search_fields = ('list_filter','title', 'author')
 admin.site.register(Book)
